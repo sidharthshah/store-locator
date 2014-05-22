a5bfbeaff176d5857f38ee0733a9d2dc3626bb04
Sidharth Shah <sidharth@fafadiatech.com>
2014-05-22 13:55:46 +0530

    added store details page

--- store_locator/store/views.py
+++ store_locator/store/views.py
@@ -1,7 +1,8 @@
-from django.views.generic.list import ListView
+from django.views.generic import ListView
 from django.utils import timezone
 
 from store.models import Store
+from product.models import Product
 
 class IndexListView(ListView):
     model = Store
@@ -10,3 +11,19 @@ class IndexListView(ListView):
         context = super(IndexListView, self).get_context_data(**kwargs)
         context['now'] = timezone.now()
         return context
+
+class StoreDetailListView(ListView):
+	model = Product
+	template_name = "store/store_detail.html"
+	context_object_name = 'products'
+
+	def get_context_data(self, **kwargs):
+	    context = super(StoreDetailListView, self).get_context_data(**kwargs)
+	    context['now'] = timezone.now()
+	    store_name = Store.objects.get(id = self.kwargs['pk'])
+	    context['store_name'] = store_name
+	    return context
+
+	def get_queryset(self):
+		pk = self.kwargs['pk']
+		return Product.objects.filter(available_in=pk)
--- store_locator/store_locator/urls.py
+++ store_locator/store_locator/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from store.views import IndexListView
+from store.views import IndexListView, StoreDetailListView
 
 urlpatterns = patterns('',
     url(r'^$', IndexListView.as_view(), name='home'),
+    url(r'^store/(?P<pk>\d+)/$', StoreDetailListView.as_view(), name='store_detail'),
     url(r'^admin/', include(admin.site.urls)),
 )
