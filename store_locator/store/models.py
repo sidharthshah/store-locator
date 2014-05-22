8c647863b7c1152290983331d04625d7a28fd699
Sidharth Shah <sidharth@fafadiatech.com>
2014-05-22 10:52:51 +0530

    refactored tempalte and model for Store page

--- store_locator/store/models.py
+++ store_locator/store/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 class Store(models.Model):
     name = models.CharField(max_length=255)
-    address_1 = models.CharField(max_length=100)
-    address_2 = models.CharField(max_length=100)
+    street = models.CharField(max_length=10)
+    avenue = models.CharField(max_length=100)
+    address_1 = models.CharField(max_length=100, blank=True, null=True)
+    address_2 = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=2)
     zipcode = models.CharField(max_length=5)
